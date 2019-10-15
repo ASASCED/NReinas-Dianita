41ced91bf454bc220a059640e3825f4357e25b35
ASASCED <jesus_heaven117@outlook.com>
2019-10-15 08:48:17 -0500

    Implementado tablero

--- src/otraprueba/OtraPrueba.java
+++ src/otraprueba/OtraPrueba.java
@@ -14,26 +14,25 @@ import javax.swing.border.Border;
 public class OtraPrueba extends JFrame {
 
     JPanel jpanel = (JPanel) this.getContentPane();
+    JLabel ex = new JLabel();
     JLabel label[] = new JLabel[8];
+    JLabel tablero[][] = new JLabel[8][8];
     Border border = BorderFactory.createLineBorder(Color.black, 1);
+    int yPosition = 0;
 
     public static void main(String[] args) {
         OtraPrueba op = new OtraPrueba();
         op.setBounds(0, 0, 500, 500);
-        System.out.println("Te quiero Diana :D");
-        System.out.println("Y yo te quiero a ti <3");
-        System.out.println("Gracias por todo");
-        System.out.println("De nada Diana no hay de que comi siempre");
         op.setVisible(true);
         op.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
 
     public OtraPrueba() {
-//        Inicializa el panel.
         for (int i = 0; i < label.length; i++) {
             label[i] = new JLabel();
             label[i].setBounds(25 + (50 * i), 25, 30, 30);
             label[i].setText("Q" + (i + 1));
+            label[i].setForeground(Color.red);
             label[i].setBorder(border);
             label[i].setHorizontalAlignment(SwingConstants.CENTER);
             label[i].addMouseMotionListener(new MouseMotionAdapter() {
@@ -44,6 +43,32 @@ public class OtraPrueba extends JFrame {
             });
             jpanel.add(label[i], null);
         }
+
+        //        Inicializa el panel.
+        for (int i = 0; i < tablero.length; i++) {
+            for (int j = 0; j < tablero.length; j++) {
+                tablero[i][j] = new JLabel();
+                tablero[i][j].setBounds(25 + (50 * j), 25 + (50 * i), 30, 30);
+                tablero[i][j].setBorder(border);
+                tablero[i][j].setBackground(Color.red);
+
+                if ((i % 2 == 0) == (j % 2 == 0)) {
+                    tablero[i][j].setBackground(Color.white);
+                } else {
+                    tablero[i][j].setBackground(Color.black);
+                }
+
+                tablero[i][j].setOpaque(true);
+                tablero[i][j].setHorizontalAlignment(SwingConstants.CENTER);
+                jpanel.add(tablero[i][j], null);
+            }
+        }
+
+        // Creamos un pequeño label extra para que imprima el resto correctamente
+        ex.setBounds(25, 25, 30, 30);
+        ex.setBorder(border);
+        ex.setBackground(Color.red);
+        jpanel.add(ex, null);
     }
 
     public void myDraggingMethod(MouseEvent evt) {
