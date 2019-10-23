97fabc3984992fa27f4e43315e2c5863798b88c9
ASASCED <jesus_heaven117@outlook.com>
2019-10-22 20:23:11 -0500

    Algoritmo implementado - Problemas con la optimizacion

--- src/otraprueba/OtraPrueba.java
+++ src/otraprueba/OtraPrueba.java
@@ -1,10 +1,13 @@
 package otraprueba;
 
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionAdapter;
 import javax.swing.BorderFactory;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
@@ -13,9 +16,11 @@ import javax.swing.SwingConstants;
 import javax.swing.WindowConstants;
 import javax.swing.border.Border;
 
-public class OtraPrueba extends JFrame {
+public class OtraPrueba extends JFrame implements ActionListener {
 
     static int reinas = 0;
+    String[][] algoritmo = new String[reinas][reinas];
+    String[][] comprobacion = new String[reinas][reinas];
     int count = 0;
     int anchoAlto = 50;
     int margen = 25;
@@ -26,6 +31,11 @@ public class OtraPrueba extends JFrame {
     JLabel tablero[][] = new JLabel[reinas][reinas];
     Border border = BorderFactory.createLineBorder(Color.black, 1);
 
+    String[] parts;
+    int x, y, cooX, cooY, newI, newJ;
+
+    JButton boton1;
+
     public static void main(String[] args) {
         reinas = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese la cantidad de reinas"));
         OtraPrueba op = new OtraPrueba();
@@ -35,6 +45,7 @@ public class OtraPrueba extends JFrame {
     }
 
     public OtraPrueba() {
+        // Declaracion de reinas
         for (int i = 0; i < label.length; i++) {
             label[i] = new JLabel();
             // r + r + r + r
@@ -51,13 +62,27 @@ public class OtraPrueba extends JFrame {
             });
             label[i].addMouseListener(new MouseAdapter() {
                 @Override
+                public void mouseReleased(MouseEvent evt) {
+                    arregloTablero(evt);
+                }
+
+                @Override
                 public void mousePressed(MouseEvent evt) {
-                    movimientoReina(evt);
+                    valoresIniciales(evt);
                 }
             });
             jpanel.add(label[i]);
+
+            for (int j = 0; j < algoritmo.length; j++) {
+                if (i == 0) {
+                    algoritmo[i][j] = Integer.toString((margen + (espacio * j))) + "|" + Integer.toString(margen);
+                } else {
+                    algoritmo[i][j] = "0|0";
+                }
+            }
         }
 
+        // Declaracion del tablero
         for (int i = 0; i < tablero.length; i++) {
             for (int j = 0; j < tablero.length; j++) {
                 tablero[i][j] = new JLabel();
@@ -76,8 +101,86 @@ public class OtraPrueba extends JFrame {
             }
         }
 
+        // Declaracion del boton
+        setLayout(null);
+        boton1 = new JButton("Finalizar");
+        boton1.setBounds(25, 3, 90, 20);
+        jpanel.add(boton1);
+        boton1.addActionListener(this);
+
         ex.setBounds(margen, margen, anchoAlto, anchoAlto);
         jpanel.add(ex);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == boton1) {
+            for (int i = 0; i < comprobacion.length; i++) {
+                for (int j = 0; j < comprobacion.length; j++) {
+                    if (algoritmo[i][j].equals("0|0")) {
+                        comprobacion[i][j] = "*";
+                    } else {
+                        comprobacion[i][j] = "+";
+                    }
+                }
+            }
+
+            imprimirArreglo(comprobacion);
+
+            int contUno = 0;
+            int contDos = 0;
+            int contTres = 0;
+
+            int diagS, diagI;
+
+            for (int i = 0; i < comprobacion.length; i++) {
+                for (int j = 0; j < comprobacion.length; j++) {
+                    if (comprobacion[i][j].equals("+")) {
+                        contUno++;
+                        if (contUno == 2) {
+                            JOptionPane.showMessageDialog(null, "Existe más de una reina en una fila");
+                            break;
+                        }
+                    }
+                    if (comprobacion[j][i].equals("+")) {
+                        contDos++;
+                        if (contDos == 2) {
+                            JOptionPane.showMessageDialog(null, "Existe más de una reina en una columna");
+                            break;
+                        }
+
+                        diagS = j - 1;
+                        diagI = j + 1;
+
+                        if (diagS < 0) {
+                            diagS = 0;
+                        } else if (diagI >= comprobacion.length) {
+                            diagI = (comprobacion.length - 1);
+                        }
+
+                        System.out.println("S/I: " + diagS + "/" + diagI);
+
+                        if (i < comprobacion.length - 1) {
+                            if (comprobacion[diagS][i + 1].equals("+") || comprobacion[diagI][i + 1].equals("+")) {
+                                JOptionPane.showMessageDialog(null, "En alguna diagnoal de las reinas existe mas de una reina");
+                                contTres = 2;
+                                break;
+                            }
+                        }
+                    }
+                }
+                if (contUno == 2 || contDos == 2 || contTres == 2) {
+                    break;
+                }
+                contUno = 0;
+                contDos = 0;
+                contTres = 0;
+            }
+            
+            if (contUno == 0 && contDos == 0 && contTres == 0) {
+                JOptionPane.showMessageDialog(null, "Felicidades, completaste el juego de N Reinas");
+            }
+        }
     }
 
     public void arrastreReina(MouseEvent evt) {
@@ -86,10 +189,52 @@ public class OtraPrueba extends JFrame {
         }
     }
 
-    public void movimientoReina(MouseEvent evt) {
+    public void valoresIniciales(MouseEvent evt) {
         if (evt.getSource() instanceof JLabel) {
+            for (int i = 0; i < algoritmo.length; i++) {
+                for (int j = 0; j < algoritmo.length; j++) {
+                    String string = algoritmo[i][j];
+                    String[] parts = string.split("\\|");
+                    this.cooX = Integer.parseInt(parts[0]);
+                    this.cooY = Integer.parseInt(parts[1]);
+
+                    if (cooX == posicion(evt)[0] && cooY == posicion(evt)[1]) {
+                        System.out.println("i/j: " + i + "/" + j);
+                        this.newI = i;
+                        this.newJ = j;
+                    }
+                }
+            }
+            x = posicion(evt)[0];
+            y = posicion(evt)[1];
+            System.out.println("X/Y: " + x + "/" + y);
+        }
+    }
+
+    public void arregloTablero(MouseEvent evt) {
+        if (evt.getSource() instanceof JLabel) {
+            int newX = (posicion(evt)[0] - x) / 50;
+            int newY = (posicion(evt)[1] - y) / 50;
+
+            System.out.println("pos X/Y: " + posicion(evt)[0] + "/" + posicion(evt)[1]);
+            System.out.println("newX: " + newX + "\nnewY: " + newY);
+
+            algoritmo[newI][newJ] = "0|0";
+            System.out.println("newI/newJ: " + newI + "/" + newJ);
+            algoritmo[newI + newY][newJ + newX] = Integer.toString(posicion(evt)[0]) + "|" + Integer.toString(posicion(evt)[1]);
+
             count++;
             System.out.println("Movimientos: " + count);
+            imprimirArreglo(algoritmo);
+        }
+    }
+
+    public void imprimirArreglo(String[][] arreglo) {
+        for (int i = 0; i < arreglo.length; i++) {
+            for (int j = 0; j < arreglo.length; j++) {
+                System.out.print(arreglo[i][j] + "    ");
+            }
+            System.out.println("");
         }
     }
 
@@ -109,19 +254,19 @@ public class OtraPrueba extends JFrame {
         int primerosMenosUltimosY = Integer.parseInt(ultimoY);
 
         if (primerosMenosUltimosX < 25) {
-            newX = 25 + (evt.getXOnScreen() - 50 - primerosMenosUltimosX);
+            newX = 25 + (evt.getXOnScreen() - espacio - primerosMenosUltimosX);
         } else if (primerosMenosUltimosX < 75) {
-            newX = 75 + (evt.getXOnScreen() - 50 - primerosMenosUltimosX);
+            newX = 75 + (evt.getXOnScreen() - espacio - primerosMenosUltimosX);
         } else {
-            newX = 25 + ((evt.getXOnScreen() - 50 + 100) - primerosMenosUltimosX);
+            newX = 25 + ((evt.getXOnScreen() - espacio + 100) - primerosMenosUltimosX);
         }
 
         if (primerosMenosUltimosY < 25) {
-            newY = 25 + (evt.getYOnScreen() - 50 - primerosMenosUltimosY);
+            newY = 25 + (evt.getYOnScreen() - espacio - primerosMenosUltimosY);
         } else if (primerosMenosUltimosY < 75) {
-            newY = 75 + (evt.getYOnScreen() - 50 - primerosMenosUltimosY);
+            newY = 75 + (evt.getYOnScreen() - espacio - primerosMenosUltimosY);
         } else {
-            newY = 25 + ((evt.getYOnScreen() - 50 + 100) - primerosMenosUltimosY);
+            newY = 25 + ((evt.getYOnScreen() - espacio + 100) - primerosMenosUltimosY);
         }
 
         int retorno[] = {newX, newY};
